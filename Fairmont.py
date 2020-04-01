93c3cda102c62c74314653839097bc72089a183c
Monroe Weber-Shirk <monroews@gmail.com>
2020-04-01 16:41:54 -0400

    Update Fairmont.py

--- Fairmont.py
+++ Fairmont.py
@@ -1,3 +1,6 @@
+import aguaclara as ac
+from aguaclara.core.units import unit_registry as u
+
 LAAWFP_Q = 480 * u.Mgal/u.day
 LAAWFP_As_C = 25 * u.ug/u.L
 LAAWFP_As_M = (LAAWFP_Q * LAAWFP_As_C).to(u.kg/u.day)
