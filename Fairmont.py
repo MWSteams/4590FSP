027cf775677fd65207b1d5710a7c762beecbda71
Monroe Weber-Shirk <monroews@gmail.com>
2020-04-01 17:04:25 -0400

    Update Fairmont.py

--- Fairmont.py
+++ Fairmont.py
@@ -1,7 +1,15 @@
 import aguaclara as ac
 from aguaclara.core.units import unit_registry as u
 
-LAAWFP_Q = 480 * u.Mgal/u.day
-LAAWFP_As_C = 25 * u.ug/u.L
-LAAWFP_As_M = (LAAWFP_Q * LAAWFP_As_C).to(u.kg/u.day)
-print('why isnt this working?')
+Q_max = (480 * u.Mgal/u.day).to(u.kL/u.s)
+Q_ave = (320 * u.ft**3/u.s).to(u.kL/u.s)
+#Q_min = need to find this value
+#N_train = 1
+
+Fe_C_ave = 13.14 * u.mg/u.L
+
+As_Cin_ave = 25 * u.ug/u.L
+As_M = (LAAWFP_Q * LAAWFP_As_C).to(u.kg/u.day)
+
+Turbidity_Cin_ave = 14 * u.NTU
+Turbidity_Cout_ave = 3 * u.NTU
