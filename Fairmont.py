e2954bfb5906a3adfd1df3066f409b1807342123
Monroe Weber-Shirk <monroews@gmail.com>
2020-04-02 08:31:57 -0400

    Update Fairmont.py

--- Fairmont.py
+++ Fairmont.py
@@ -1,15 +1,53 @@
+"""
+Use this file to hold parameters that are in the SOW or elsewhere that
+influence the design of the entire plant. This provides an easy mechanism
+for all of us to share the same assumptions.
+
+Variable naming convention
+* Use lower case letters almost always. Exception is element abbrevations
+* separate words with underscore
+* start general and work down to the specific variable if context is required
+
+The flow through a single plate settler would be:
+sed_plate_q
+
+The width of the total area dedicated to flocculators would be:
+floc_w
+
+The width of one floc tank would be:
+floc_tank_w
+
+"""
+
 import aguaclara as ac
 from aguaclara.core.units import unit_registry as u
 
-Q_max = (480 * u.Mgal/u.day).to(u.kL/u.s)
-Q_ave = (320 * u.ft**3/u.s).to(u.kL/u.s)
-#Q_min = need to find this value
-#N_train = 1
+"""Constants or assumptions from the SOW"""
 
-Fe_C_ave = 13.14 * u.mg/u.L
+# Plant flow variability
+q_max = (480 * u.Mgal/u.day).to(u.kL/u.s)
+q_ave = (320 * u.ft**3/u.s).to(u.kL/u.s)
+# q_min = need to find this value
 
-As_Cin_ave = 25 * u.ug/u.L
-As_M = (Q_ave * As_Cin_ave).to(u.kg/u.day)
+# Turbidity assumptions
+turbidity_c_in_ave = 14 * u.NTU
+turbidity_c_out_ave = 3 * u.NTU
 
-Turbidity_Cin_ave = 14 * u.NTU
-Turbidity_Cout_ave = 3 * u.NTU
+# Coagulant assumptions
+# This is the iron concentration, not the FeCl3 concentration
+Fe_c_ave = 13.14 * u.mg/u.L
+
+# Arsenic
+As_c_in_ave = 25 * u.ug/u.L
+As_m = (q_ave * As_c_in_ave).to(u.kg/u.day)
+
+"""Regulations that influence the design"""
+
+# waiting for some of these!
+
+"""
+Assumptions that we are making that influence the entire design.
+This will include the number of treatment trains
+and more as our design progresses.
+"""
+# n_train = x
